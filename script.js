023fda70d5b915d51e20ccea076dbd887f7a694b
jitender1222 <93830004+jitender1222@users.noreply.github.com>
2022-10-18 09:53:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const input=document.getElementById('input');
+
+const btn=document.querySelector('.btn');
+
+let first=0,last=0,indentify=1;
+
+
+btn.addEventListener('click',()=>{
+
+    if(input.value.length==0){
+     alert("Enter value first") ;
+    }
+    let s=input.value.toString();
+     last=s.length-1;
+    
+     while(first<=last){
+        if(s[first]!==s[last]){
+            console.log("not a palindrome");
+            indentify=0;
+            input.value=" ";
+            break;
+        }
+        first++;
+        last--;
+    }
+    if(indentify===1){
+        console.log("its a palindrome");
+        input.value=" ";
+    }
+})
